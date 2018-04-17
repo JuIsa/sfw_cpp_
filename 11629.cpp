cec3e6e33236fe4c755aaf36a795abbe933efdf8
JuIsa <Isami.dono@gmail.com>
2018-04-18 00:20:03 +0600

    new
    one

--- 11629.cpp
+++ 11629.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <unordered_map>
 #include <utility>
+#include <sstream>
+#include <vector>
+
 using namespace std;
 
 int main()
@@ -8,37 +11,73 @@ int main()
 	int f,s;
 	cin>>f>>s;
 	
-	unordered_map<string, double> mym;
+	unordered_map<string, int> mym;
 	for(int i=0; i<f;i++){
 		string st;
-		double d;
+		int a;
+		char ch;
+		int b;
 		cin>>st;
-		cin>>d;
-		mym.insert(pair<string,double>(st,d));
+		cin>>a;
+		cin>>ch;
+		cin>>b;
+		mym.insert(pair<string,double>(st,a*10+b));
 	}
 	
-	for(int i=0; i<s;i++){
+	for(int i=0; i<s+1;i++){
 		string s;
-		vector<string> v;
+		
 		getline(cin, s);
 		string word;
 		istringstream sinp(s);
-		bool plus = false;
-		int point =0;
-		double res =0;
+		
+		int sum =0;
+		int compare=0;
+		
 		while(sinp>>word){
-			if(plus){
-				res=v[point]+v[point-1];
-				plus=false;
-			}
-			if(word=="+"){
-				plus=true;
-			}
-			if(word=="<="){
+			if(word==">"){
+				sinp>>compare;
 				
+				if(sum>compare*10)
+					printf("Guess #%d was correct.\n", i);
+				else
+					printf("Guess #%d was incorrect.\n", i);
+				break;
+			}else if(word==">="){
+				sinp>>compare;
+				
+				if(sum>=compare*10)
+					printf("Guess #%d was correct.\n", i);
+				else
+					printf("Guess #%d was incorrect.\n", i);
+			}else if(word=="<"){
+				sinp>>compare;
+				
+				if(sum<compare*10)
+					printf("Guess #%d was correct.\n", i);
+				else
+					printf("Guess #%d was incorrect.\n", i);
+			}else if(word=="<="){
+				sinp>>compare;
+				
+				if(sum<=compare*10)
+					printf("Guess #%d was correct.\n", i);
+				else
+					printf("Guess #%d was incorrect.\n", i);
+			}else if(word=="="){
+				sinp>>compare;
+				
+				if(sum==compare*10)
+					printf("Guess #%d was correct.\n", i);
+				else
+					printf("Guess #%d was incorrect.\n", i);
+			}else if(word=="+"){
+				int aaa=5;
+			}else{
+				auto it= mym.find(word);
+				sum+=it->second;
+				//cout<< sum<<endl;
 			}
-			v.push_back(word);
-			point++;
 			
 		}
 		
