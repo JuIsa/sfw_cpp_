84ee74e7fb3e072ce7d0735ff2fabc8eecff8e05
JuIsa <Isami.dono@gmail.com>
2018-05-07 23:07:16 +0600

    Project 2
    a

--- 10919.cpp
+++ 10919.cpp
@@ -5,15 +5,19 @@
 using namespace std;
 
 int main(){
-	int num_of_course, num_of_cat;
+	int num_of_course, num_of_cat;   // cat - category
 	while(cin>>num_of_course>>num_of_cat and num_of_course!=0){
+	
 	vector<int> courses;
+	
 	for(int i=0; i<num_of_course;i++){
 		int x;cin>>x;
 		courses.push_back(x);
 	}
-	bool yes = true;
+	
+	bool yes = true; // see use in the end
 	for(int j=0;j<num_of_cat;j++){
+		//read integers 
 		int num_of_req; cin>>num_of_req;
 		int num_to_take; cin>>num_to_take;
 		vector<int> req;
@@ -23,13 +27,17 @@ int main(){
 		}
 		int count=0;
 		sort(courses.begin(), courses.end());
+		//sort to enable binary_search(its necessary condition)
+		
 		for(int i=0;i<num_of_req; i++){
-			if(binary_search(courses.begin(), courses.end(), req[i])){
+			if(binary_search(courses.begin(), courses.end(), req[i])){ //first two parts is a range of search,
+//			the last is value we are looking for
 				count++;
 			}
 		}
-		if(count<num_to_take){
-			yes=false;
+		
+		if(count<num_to_take){ //operator less to check cases when student doesnt take enouhg courses
+			yes=false; 
 		}
 			
 	}
